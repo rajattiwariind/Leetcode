c31cd227e90f6224ebc20d8f5e71bac6c2521796
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2024-11-24 18:20:29 +0530

    Create majorityElement.js

---
+++ HashTable/majorityElement.js
@@ -0,0 +1,11 @@
+function majorityElement(nums) {
+    let candidate = null;
+    let count = 0;
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
+}
