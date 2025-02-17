604416a485cb15bace8f873010bea6b4f5e45ef0
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2025-02-17 18:41:56 +0530

    Create Letter Tile Possibilities.py

---
+++ DFS/Letter Tile Possibilities.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def numTilePossibilities(self, tiles):
+        def backtrack(counter):
+            count = 0
+            for char in counter:
+                if counter[char] > 0:
+                    count += 1
+                    counter[char] -= 1  
+                    count += backtrack(counter)  
+                    counter[char] += 1  
+            return count
+        
+        counter = Counter(tiles)  
+        return backtrack(counter)
+
+
+
